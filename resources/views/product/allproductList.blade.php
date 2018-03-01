afa7acd5515c0d021335fd5273b9e3dd4ac66cd7
rumi <mujtaba.rumi1@gmail.com>
2018-03-01 17:53:37 +0600

    rumi

--- resources/views/product/allproductList.blade.php
+++ resources/views/product/allproductList.blade.php
@@ -13,12 +13,12 @@
     </header>
 
 
-    <div class="table table-responsive" style="padding: 10px; background-color:white";>
+    <div  style="padding: 10px; background-color:white";>
 
         <div class="row">
-        <div class="col-md-3 dropdown">
+        <div class="col-md-4 dropdown">
             <label class="form-control-label">Status</label> <br>
-            <select class="form-control" id="status" name="status" onchange="status(this.value)">
+            <select class="form-control" id="status" name="status">
                 <option selected value="">--Select Status--</option>
                 <?php for ($i=0;$i<count(Status);$i++){?>
 
@@ -29,7 +29,7 @@
             </select>
 
         </div>
-        <div class="col-md-3 dropdown">
+        <div class="col-md-4 dropdown">
             <label class="form-control-label">Category</label> <br>
             <select class="form-control" id="status" name="status" onchange="status(this.value)">
                 <option value="">--Select Status--</option>
@@ -40,24 +40,25 @@
             </select>
 
         </div>
-        {{--<div class="col-md-3 dropdown">--}}
-            {{--<label class="form-control-label">Product Name</label> <br>--}}
-            {{--<select class="form-control" id="status" name="status" onchange="status(this.value)">--}}
-                {{--<option value="">--Select Status--</option>--}}
-                {{--@foreach($productsList as $products)--}}
-                    {{--<option value="{{$products->productId}}">{{$products->productName}}</option>--}}
-                {{--@endforeach--}}
-            {{--</select>--}}
-        {{--</div>--}}
 
-            <div class="col-md-3 form-group">
-                <label class="form-control-label" >Search</label>
-                <input name="line2" type="text" class="form-control required validate" onkeyup="search(this.value)">
-            </div>
+        <div class="col-md-4 dropdown">
+            <label class="form-control-label">Product Name</label> <br>
+            <select class="form-control" id="status" name="status" onchange="status(this.value)">
+                <option value="">--Select Status--</option>
+                @foreach($productsList as $products)
+                    <option value="{{$products->productId}}">{{$products->productName}}</option>
+                @endforeach
+            </select>
+        </div>
+
+            {{--<div class="col-md-3 form-group">--}}
+                {{--<label class="form-control-label" >Search</label>--}}
+                {{--<input name="line2" type="text" class="form-control required validate" onkeyup="search(this.value)">--}}
+            {{--</div>--}}
 
         </div>
 
-
+        <div class="table table-responsive" style="margin-top: 20px">
         <table id="allProductList" class="table table-bordered table-striped">
             <thead>
             <tr>
@@ -73,37 +74,12 @@
                 <th>Action</th>
             </tr>
             </thead>
-            <tbody>
-            {{--@foreach($productsList as $products)--}}
-            {{--<tr>--}}
-                {{--<td><input type="checkbox"></td>--}}
-                {{--<td>{{$products->categoryName}}</td>--}}
-                {{--<td>{{$products->style}}</td>--}}
-                {{--<td>{{$products->sku}}</td>--}}
-                {{--<td>{{$products->productName}}</td>--}}
-                {{--<td>{{$products->brand}}</td>--}}
-                {{--<td>{{$products->status}}</td>--}}
-                {{--<td>{{$products->LastExportedBy}}</td>--}}
-                {{--<td>{{$products->LastExportedDate}}</td>--}}
-
-                {{--<td>--}}
-                    {{--<a href="{{route('product.edit',['id' => $products->productId])}}"> <i class="fa fa-pencil-square-o" aria-hidden="true" style="color: red"></i></a> &nbsp;&nbsp;&nbsp;--}}
-                    {{--<a  href="{{route('product.destroy',['id' => $products->productId])}}" id="myBtn2" onClick="return confirm('Are you sure delete this product?')">--}}
-                        {{--<i class="fa fa-trash-o" aria-hidden="true" style="color: red"></i></a> &nbsp;&nbsp;&nbsp;--}}
-                {{--</td>--}}
-
-            {{--</tr>--}}
-            {{--@endforeach--}}
-
-            </tbody>
-
 
         </table>
 
+        </div>
+
         <a href="csv/product.csv" onclick="return myfunc()" download> <button class="btn btn-danger"  >Export Products file</button></a>
-
-
-
 
     </div>
 @endsection
@@ -119,17 +95,17 @@
 
     {{--<script src="{{url('cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js')}}"></script>--}}
     {{--<script src="{{url('cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js')}}"></script>--}}
-    
+
 
     {{--<script src="{{url('cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js')}}"></script>--}}
 
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
     <script>
-
+        var table;
         $(document).ready(function() {
             var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
-            $('#allProductList').DataTable({
+            table = $('#allProductList').DataTable({
 
                 processing: true,
                 serverSide: true,
@@ -137,7 +113,12 @@
                 "ajax":{
                     "url": "{!! route('product.data') !!}",
                     "type": "POST",
-                    "data":{ _token: "{{csrf_token()}}"}
+
+                    "data":{
+                        _token: "{{csrf_token()}}",
+                        status : $('#status').val(),
+
+                    },
                 },
                 columns: [
                     { data: 'productId', name: 'productId' },
@@ -151,7 +132,17 @@
                     { data: 'LastExportedDate', name: 'LastExportedDate' },
                     { data: 'LastExportedDate', name: 'LastExportedDate' },
 
-                ]
+                ],
+
+            });
+            $('#status').change(function(){ //button filter event click
+                table.search("").draw(); //just redraw myTableFilter
+                table.ajax.reload();  //just reload table
+
+//                var status = $('#status').val();
+//                alert(status);
+
+
             });
 
         });
