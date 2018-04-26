72b0f108eefbdb052a5df455f1e7d685c219fa71
SknightS <md.sakibrahman@gmail.com>
2018-04-26 18:52:07 +0600

    Sakib

--- resources/views/offer/serverCSVPriceAndQuantityForProductList.blade.php
+++ resources/views/offer/serverCSVPriceAndQuantityForProductList.blade.php
@@ -4,9 +4,9 @@
         <th>sku</th>
         <th>price</th>
         <th>quantity</th>
-        <th>state</th>
         <th>product-id-type</th>
         <th>product-id</th>
+        <th>state</th>
         <th>min-quantity-alert</th>
     </tr>
     </thead>
@@ -16,9 +16,9 @@
         <td>{{$list['sku']}}</td>
         <td>{{$list['price']}}</td>
         <td>{{$list['stockQty']}}</td>
-        <td>11</td>
         <td>SHOP_SKU</td>
         <td>{{$list['product-Id']}}</td>
+        <td>11</td>
         <td>{{$list['minQtyAlert']}}</td>
     </tr>
     @endforeach
